45c228235d7d38fa256798429cc7782b3d32d2d3
poopijay <jaymarkmunda26@gmail.com>
2022-04-20 23:51:50 +0800

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="py-24 text-5xl">
+      <h1>
+        A strategic approach to website design <br /> and development
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { ImMenu, ImCross } from "react-icons/im";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div>
+      <nav className="relative flex justify-between items-center ">
+        <h1 className="font-bold text-xl">Tech Agency</h1>
+        <ul className="flex justify-end items-center md:hidden">
+          <li className="mx-5">Work</li>
+          <li className="mx-5">Services</li>
+          <li className="mx-5">Insights</li>
+          <li className="mx-5">About us</li>
+          <button className="mx-5 cursor-pointer bg-slate-500 text-white hover:bg-slate-600">
+            Get Started
+          </button>
+        </ul>
+
+        {!toggleMenu ? (
+          <ImMenu
+            className="hidden md:block w-5 cursor-pointer absolute top-2 right-2 z-10"
+            onClick={() => setToggleMenu(!toggleMenu)}
+          />
+        ) : (
+          <ImCross
+            className="hidden md:block w-5 cursor-pointer absolute top-2 right-2 z-10"
+            onClick={() => setToggleMenu(!toggleMenu)}
+          />
+        )}
+
+        {/* Mobile Menu */}
+        <ul
+          className={
+            !toggleMenu
+              ? "hidden"
+              : " bg-white absolute drop-shadow-md top-0 left-0 w-full h-[80vh] flex flex-col justify-evenly items-center py-4"
+          }>
+          <li className="my-4">Work</li>
+          <li className="my-4">Services</li>
+          <li className="my-4">Insights</li>
+          <li className="my-4">About us</li>
+          <button className="my-5 cursor-pointer bg-slate-500 text-white hover:bg-slate-600">
+            Get Started
+          </button>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
