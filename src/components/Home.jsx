17c6d0a4bf41b0f1af062c36594de7dd9251c345
poopijay <jaymarkmunda26@gmail.com>
2022-04-22 00:38:51 +0800

    fix navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import businessImg1 from "../assets/businessman-working-stylus-pen-and-digital.jpg";
+import businessImg2 from "../assets/meeting-concept.jpg";
 
 const Home = () => {
   return (
-    <div className="py-24 text-5xl">
-      <h1>
+    <div className="py-36">
+      <h1 className="text-5xl font-bold">
         A strategic approach to website design <br /> and development
       </h1>
+      <div className="my-36 grid grid-cols-2 grid-rows-2 gap-24">
+        <div>
+          <h2 className="text-4xl mb-4">
+            Solving varying problems with technological solutions
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi unde
+            magnam, esse, maxime dolorum quidem voluptatem dignissimos aliquam
+            placeat asperiores incidunt omnis quos veniam! Explicabo illum,
+            corporis ullam dolores velit labore illo, ratione doloribus dolorem
+            numquam, aliquid assumenda nisi laboriosam?
+          </p>
+          <button className="my-5 cursor-pointer border-2 border-slate-500 hover:text-white hover:bg-slate-600">
+            Learn more
+          </button>
+        </div>
+        <img src={businessImg1} alt="businessman" />
+        <img src={businessImg2} alt="designer" />
+        <div>
+          <h2 className="text-4xl mb-4">
+            Designing success through creative web design
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi unde
+            magnam, esse, maxime dolorum quidem voluptatem dignissimos aliquam
+            placeat asperiores incidunt omnis quos veniam! Explicabo illum,
+            corporis ullam dolores velit labore illo, ratione doloribus dolorem
+            numquam, aliquid assumenda nisi laboriosam?
+          </p>
+          <button className="my-5 cursor-pointer border-2 border-slate-500 hover:text-white hover:bg-slate-600">
+            Learn more
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div>
-      <nav className="relative flex justify-between items-center ">
-        <h1 className="font-bold text-xl">Tech Agency</h1>
+    <div className="sticky top-0 left-0 bg-white">
+      <nav className="h-16 flex justify-between items-center">
+        <h1 className="mx-5 font-bold drop-shadow-md text-xl">Tech Agency</h1>
         <ul className="flex justify-end items-center md:hidden">
           <li className="mx-5">Work</li>
           <li className="mx-5">Services</li>
@@ -20,12 +20,12 @@ const Navbar = () => {
 
         {!toggleMenu ? (
           <ImMenu
-            className="hidden md:block w-5 cursor-pointer absolute top-2 right-2 z-10"
+            className="hidden md:block w-5 cursor-pointer absolute top-6 right-2 z-10"
             onClick={() => setToggleMenu(!toggleMenu)}
           />
         ) : (
           <ImCross
-            className="hidden md:block w-5 cursor-pointer absolute top-2 right-2 z-10"
+            className="hidden md:block w-5 cursor-pointer absolute top-6 right-2 z-10"
             onClick={() => setToggleMenu(!toggleMenu)}
           />
         )}
@@ -35,13 +35,23 @@ const Navbar = () => {
           className={
             !toggleMenu
               ? "hidden"
-              : " bg-white absolute drop-shadow-md top-0 left-0 w-full h-[80vh] flex flex-col justify-evenly items-center py-4"
+              : " bg-white absolute drop-shadow-md top-0 left-0 w-full h-screen flex flex-col justify-evenly items-center py-4"
           }>
-          <li className="my-4">Work</li>
-          <li className="my-4">Services</li>
-          <li className="my-4">Insights</li>
-          <li className="my-4">About us</li>
-          <button className="my-5 cursor-pointer bg-slate-500 text-white hover:bg-slate-600">
+          <li className="my-4" onClick={() => setToggleMenu(false)}>
+            Work
+          </li>
+          <li className="my-4" onClick={() => setToggleMenu(false)}>
+            Services
+          </li>
+          <li className="my-4" onClick={() => setToggleMenu(false)}>
+            Insights
+          </li>
+          <li className="my-4" onClick={() => setToggleMenu(false)}>
+            About us
+          </li>
+          <button
+            className="my-5 cursor-pointer bg-slate-500 text-white hover:bg-slate-600"
+            onClick={() => setToggleMenu(false)}>
             Get Started
           </button>
         </ul>
